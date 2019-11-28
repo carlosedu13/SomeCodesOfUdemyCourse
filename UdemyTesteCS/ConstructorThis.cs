3343c9c5bff11179bbbb223d4d34dcc6517fbfb2
carlos.silva <carlos.silva@indigosoft.com.br>
2019-11-28 17:54:02 -0300

    OO Heranca

---
+++ UdemyTesteCS/ConstructorThis.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UdemyTesteCS
+{
+    public class Animal
+    {
+        public string Nome { get; set; }
+
+        public Animal(string nome)
+        {
+            Nome = nome;
+        }
+    }
+
+    public class Cachorro : Animal
+    {
+        public double Altura { get; set; }
+
+        public Cachorro(string nome) : base(nome)
+        {
+            Console.WriteLine($"Cachorro {nome} inicializado");
+        }
+
+        public Cachorro(string nome, double altura) : this(nome)
+        {
+            Altura = altura;
+        }
+    }
+
+    class ConstructorThis
+    {
+    }
+}
